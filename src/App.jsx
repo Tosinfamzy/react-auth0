86bb1d18a52cecc7e1ff8c2412d5b40c3be55b00
Tosinfamzy <famurewatosin@gmail.com>
2023-10-03 22:24:43 +0100

    add profile information

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
 import LogoutButton from './components/LogoutButton';
 import LoginButton from './components/LoginButton';
+import { useAuth0 } from '@auth0/auth0-react';
+import Profile from './components/Profile';
 
 function App() {
+	const { isLoading, error } = useAuth0();
 	return (
 		<>
 			<main>
 				<h1>Auth0 Login</h1>
-				<LoginButton />
-				<LogoutButton />
+				{error && <p>Authentication Error</p>}
+				{!isLoading && (
+					<>
+						<LoginButton />
+						<LogoutButton />
+						<Profile />
+					</>
+				)}
 			</main>
 		</>
 	);
